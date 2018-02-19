a8c6fe77d14d64b11230e4d00c6ca572ba382a29
andrea-isabel <andrea19.93.15@gmail.com>
2018-02-19 13:03:18 -0500

    cambios en modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-  let $pubText = $('#pub-text'),
-    $pubImg = $('#pub-img');
-
-  $($pubText, $pubImg).on('click', function(event) {
-    // event.preventDefault();
-    let $textarea = `<div class="input-field col s12">
-                      <textarea id="textarea1" class="materialize-textarea"></textarea>
-                      <label for="textarea1">Textarea</label>
+  $('.modal-trigger .material-icons').on('click', function(event) {
+    let $pubContent = $('#pub-content'),
+      $textarea = `<div class="input-field col s12">
+                    <textarea id="textarea1" class="materialize-textarea"></textarea>
+                    <label for="textarea1">Textarea</label>
+                  </div>`,
+      $inputFile = `<div class="file-field input-field">
+                      <div class="btn">
+                        <span>File</span>
+                        <input type="file">
+                      </div>
+                      <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                      </div>
+                    </div>`,
+      $datePicker = `<div class="input-field col s12">
+                      <input id="datepicker" placeholder="Select a date" type="text">
                     </div>`;
 
-    $('#pub-content').html($textarea);
-    console.log($('#pub-content'));
+    switch (true) {
+    case $(event.target).attr('id') === 'pub-text' :
+      $pubContent.html($textarea);
+      break;
+    case $(event.target).attr('id') === 'pub-img' : 
+      $pubContent.html($inputFile);
+      break;
+    case $(event.target).attr('id') === 'pub-event' :
+      $pubContent.html($datePicker);
+      console.log(event.target);
+    } 
+
+    $('#datepicker').pickadate();
   });
 
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
