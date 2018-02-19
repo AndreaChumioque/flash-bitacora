76a209848a13beb5a05ae7f8544ee6817a7abde2
andrea-isabel <andrea19.93.15@gmail.com>
2018-02-19 11:55:40 -0500

    agregando textarea

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+  let $pubText = $('#pub-text'),
+    $pubImg = $('#pub-img');
+
+  $($pubText, $pubImg).on('click', function(event) {
+    // event.preventDefault();
+    let $textarea = `<div class="input-field col s12">
+                      <textarea id="textarea1" class="materialize-textarea"></textarea>
+                      <label for="textarea1">Textarea</label>
+                    </div>`;
+
+    $('#pub-content').html($textarea);
+    console.log($('#pub-content'));
+  });
+
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
